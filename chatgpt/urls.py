4e25ab2c4584006db192ddb997f79a692891bbaa
nunssuby <nunssuby@gmail.com>
2023-05-06 23:41:38 +0900

    안꾸민버전 chatgpt

--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'chatgpt'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.index, name='index'),
+    path('chat', views.chat, name='chat'),
 ]
