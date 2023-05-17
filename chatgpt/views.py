b4d57484a741d25e5c8885d91247d12021d0a9c8
nunssuby <nunssuby@gmail.com>
2023-05-17 17:48:44 +0900

    .

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-
-from chatgpt.views import chatGPT
-
-
+import openai
 # Create your views here.
+
+openai.api_key = "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"
+
+
+#chatGPT에게 채팅 요청 API
+def chatGPT(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    print(completion)
+    result = completion.choices[0].message.content
+    return result
+
+#chatGPT에게 그림 요청 API
+def imageGPT(prompt):
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="256x256"
+    )
+    result =response['data'][0]['url']
+    return result
 
 def index(request):
     return render(request, 'gpt/index.html')
