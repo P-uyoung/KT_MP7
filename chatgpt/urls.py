b45b1ca6f520451076306bcc0fd088ce3b46fbb3
WooyoungPark <wooyoung9654@gmail.com>
2023-12-05 14:03:03 +0900

    init

---
+++ chatgpt/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'chatgpt'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('chat', views.chat, name='chat'),
+]
---
+++ selfchatgpt/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from openai import OpenAI
+
+# client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
+
+
+
+# Create your views here.
+
+
+
+
+#chatGPT에게 채팅 요청 API
+def chatGPT(prompt):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}])
+    print(completion)
+    result = completion.choices[0].message.content
+    return result
+
+#chatGPT에게 그림 요청 API
+def imageGPT(prompt):
+    response = client.images.generate(prompt=prompt,
+    n=1,
+    size="256x256")
+    result =response['data'][0]['url']
+    return result
+
+def index(request):
+    return render(request, 'gpt/index.html')
+
+def chat(request):
+    #post로 받은 question
+    prompt = request.POST.get('question')
+
+
+    #type가 text면 chatGPT에게 채팅 요청 , type가 image면 imageGPT에게 채팅 요청
+    result = chatGPT(prompt)
+
+    context = {
+        'question': prompt,
+        'result': result
+    }
+
+    return render(request, 'gpt/result.html', context) 
