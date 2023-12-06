4bc4fd098e80cd0005e1b69d53739d6ef5333c8d
WooyoungPark <wooyoung9654@gmail.com>
2023-12-06 12:24:24 +0900

    챗봇 추가 기능 - 이전 대화 보여주기

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from openai import OpenAI
 from .models import ChatHistory
 
-client = OpenAI(api_key="")
+client = OpenAI(api_key="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")
 # Create your views here.
 
 
@@ -10,9 +10,19 @@ client = OpenAI(api_key="")
 def chatGPT(prompt):
     completion = client.chat.completions.create(model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": prompt}])
+    print(completion)
     result = completion.choices[0].message.content
-    ChatHistory.objects.create(user_message=prompt, gpt_response=result)
     return result
+
+def your_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('question')
+        result = chatGPT(prompt)
+    else:
+        result = None
+
+    chat_history = ChatHistory.objects.all().order_by('-created_at')  # 최신 대화부터 표시
+    return render(request, 'your_template.html', {'chat_history': chat_history, 'result': result})
 
 
 #chatGPT에게 그림 요청 API
@@ -30,13 +40,21 @@ def chat(request):
     #post로 받은 question
     prompt = request.POST.get('question')
 
-
     #type가 text면 chatGPT에게 채팅 요청 , type가 image면 imageGPT에게 채팅 요청
     result = chatGPT(prompt)
+    
+    # 새로운 대화 기록을 데이터베이스에 저장
+    ChatHistory.objects.create(user_message=prompt, gpt_response=result)
+    
+    # 데이터베이스에서 모든 대화 기록을 조회
+    chat_history = ChatHistory.objects.all().order_by('created_at')
+    sessions = ChatHistory.objects.values_list('session_id', flat=True).distinct()
 
     context = {
         'question': prompt,
-        'result': result
+        'result': result,
+        'chat_history': chat_history,
+        'sessions' : sessions
     }
 
     return render(request, 'gpt/result.html', context) 
