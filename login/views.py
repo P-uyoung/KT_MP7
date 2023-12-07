23a26dbdb340ee2daad267d8b2eb1318ff35fa94
woojin <nicholasbear@naver.com>
2023-12-07 13:25:50 +0900

    fixed login

--- login/views.py
+++ login/views.py
@@ -8,22 +8,24 @@ from django.contrib import messages
 #로그인
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username',None)
-        password = request.POST.get('password',None)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if username is not None:
+            existing_user = User.objects.filter(username=username).exists()
             # Redirect to a success page or render a different template
-            return redirect('login:success')
+            if existing_user:
+                request.session.clear()
+                request.session['username'] = username
+                return redirect('home')
+            else:
+                pass
         else:
             #messages.error(request, 'Invalid login credentials')
             #return render(request, 'login/index.html')
             pass
             
     return render(request, 'login/index.html')
-# 성공했을시
-def success_view(request):
-    return render(request, 'login/success.html')
 
 # 계정 추가
 def add(request):
@@ -39,12 +41,14 @@ def add(request):
             return JsonResponse({'message': message})
         else:
             new_user = User.objects.create(username=username, email=email, password=password)
+            new_user.save()
             message = "added"
-            return JsonResponse({'message': message})
+            return render(request, 'login/index.html')
     else:
         message = "input all info"
         return JsonResponse({'message': message})
     
-    
-    
-
+def logout(request):
+    request.session.clear()
+    #request.session['username'] = username
+    return redirect('home')
